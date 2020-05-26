e7159440bb357957db08962192c93a9537e4f3da
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-26 11:23:17 -0500

    finished logins styling, and dom

--- src/event_listeners.js
+++ src/event_listeners.js
@@ -1,1 +1,30 @@
 // Event listeners go here
+
+const main = () => {
+    userBtnListener()
+    businessBtnListener()
+}
+
+const userBtnListener = () => {
+    let userBtn = document.getElementById('userBtn')
+    let loginPage = document.getElementById('loginPage')
+    let loginPageUser = document.getElementById('loginPageUser')
+
+    userBtn.addEventListener('click', (event) => {
+        loginPage.hidden = true
+        loginPageUser.hidden = false
+    })
+}
+
+const businessBtnListener = () => {
+    let businessBtn = document.getElementById('businessBtn')
+    let loginPage = document.getElementById('loginPage')
+    let loginPageBusiness = document.getElementById('loginPageBusiness')
+
+    businessBtn.addEventListener('click', (event) => {
+        loginPage.hidden = true
+        loginPageBusiness.hidden = false
+    })
+}
+
+main()
