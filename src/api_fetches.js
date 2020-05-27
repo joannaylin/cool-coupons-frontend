98951088fdbd2854a591a07cd0ac90b3d2cfd569
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-27 17:18:33 -0500

    added edit functionality

--- src/api_fetches.js
+++ src/api_fetches.js
@@ -7,6 +7,7 @@ const mainAPIFetches = () => {
   navbarSearch();
   businessLogin();
   createCoupon();
+  editCoupon();
   deleteCoupon();
 };
 
@@ -150,25 +151,28 @@ const businessLogin = () => {
   let loginsContainer = document.getElementById("loginsContainer");
   let couponContainer = document.getElementById("couponContainer");
   let createCouponBtn = document.getElementById("createCouponBtn");
+  let validationText = document.getElementById('validationText')
 
   businessLoginForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    let businessValue = event.target[0].value;
 
-    fetch("http://localhost:3000/businesses")
-      .then((resp) => resp.json())
-      .then((businesses) => {
-        businesses.forEach((business) => {
-          if (business.name === businessValue) {
-            currentBusiness = business;
+    fetch('http://localhost:3000/businesses')
+      .then(response => {
+        return response.json()
+      })
+      .then(businesses => {
+        businesses.forEach(business => {
+          if (business.name === event.target[0].value) {
+            currentBusiness = business
             loginsContainer.hidden = true;
             couponContainer.hidden = false;
             createCouponBtn.hidden = false;
-
             renderBusinessCoupons(business);
+          } else {
+            validationText.hidden = false
           }
-        });
-      });
+        })
+      })
   });
 };
 
@@ -181,8 +185,8 @@ const renderBusinessCoupons = (business) => {
       <h3>Code: ${coupon.code}</h3>
       <p>${coupon.likes} Likes</p>
       <p>Exp. date: ${coupon.expiration_date}</p>
-      <button class="btn-lg" id="editCouponBtn">Edit Coupon</button>
-      <button class="btn-lg" id="deleteCouponBtn">Delete Coupon</button>
+      <button class="btn-lg" id="editCouponBtn" data-id="${coupon.id}">Edit Coupon</button>
+      <button class="btn-lg" id="deleteCouponBtn" data-id="${coupon.id}">Delete Coupon</button>
     </div>`;
     couponContainer.innerHTML += couponCard;
   });
@@ -204,7 +208,7 @@ const createCoupon = () => {
       name: event.target[0].value,
       code: event.target[1].value,
       expiration_date: event.target[2].value,
-      business_id: currentBusiness.id,
+      business_id: currentBusiness.id
     };
 
     const configObj = {
@@ -237,20 +241,78 @@ const renderBusinessCoupon = (coupon) => {
   <div class="coupon-card" data-id="${coupon.id}">
     <h1>${coupon.name}</h1>
     <h3>Code: ${coupon.code}</h3>
-    <p>${coupon.likes.length} Likes</p>
+    <p>${coupon.likes} Likes</p>
     <p>Exp. date: ${coupon.expiration_date}</p>
-    <button class="btn-lg" id="editCouponBtn">Edit Coupon</button>
-    <button class="btn-lg" id="deleteCouponBtn">Delete Coupon</button>
+    <button class="btn-lg" id="editCouponBtn" data-id="${coupon.id}">Edit Coupon</button>
+    <button class="btn-lg" id="deleteCouponBtn" data-id="${coupon.id}">Delete Coupon</button>
   </div>`;
   couponContainer.innerHTML += couponCard;
 };
+
+const editCoupon = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.id === "editCouponBtn") {
+      let createCouponBtn = document.getElementById("createCouponBtn");
+      let couponContainer = document.getElementById("couponContainer");
+      let editCouponContainer = document.getElementById("editCouponContainer");
+
+      createCouponBtn.hidden = true
+      couponContainer.hidden = true
+      editCouponContainer.hidden = false
+      
+      let editBtn = event.target;
+      let couponCard = editBtn.parentElement
+      let couponId = editBtn.dataset.id
+
+      let editCouponForm = document.getElementById('editCouponForm')
+
+      editCouponForm[0].value = couponCard.firstElementChild.innerText
+      editCouponForm[1].value = couponCard.getElementsByTagName('h3')[0].innerText.split(' ')[1]
+      editCouponForm[2].value = couponCard.getElementsByTagName('p')[1].innerText.split(' ')[2]
+
+      editCouponForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const editCouponURL = `http://localhost:3000/coupons/${couponId}`
+
+        const formData = {
+          name: event.target[0].value,
+          code: event.target[1].value,
+          expiration_date: event.target[2].value,
+          business_id: currentBusiness.id
+        }
+
+        const formObj = {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accepts': 'application/json' 
+          },
+          body: JSON.stringify({coupon: formData})
+        }
+
+        fetch(editCouponURL, formObj)
+          .then(response => {
+            return response.json()
+          })
+          .then(coupon => {
+            createCouponBtn.hidden = false;
+            couponCard.remove()
+            couponContainer.hidden = false;
+            editCouponContainer.hidden = true;
+
+            renderBusinessCoupon(coupon);
+          })
+      })
+    }
+  })
+}
 
 const deleteCoupon = () => {
   document.addEventListener("click", (event) => {
     if (event.target.id === "deleteCouponBtn") {
       let deleteBtn = event.target;
       let couponCard = deleteBtn.parentElement;
-      let couponId = couponCard.dataset.id;
+      let couponId = deleteBtn.dataset.id;
 
       const deleteCouponURL = `http://localhost:3000/coupons/${couponId}`;
 
